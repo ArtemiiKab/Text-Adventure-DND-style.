bf894b691140cf45d4dd93f1a89264f3f419581d
Artemii_Kabanov <52568994+ArtemiiKab@users.noreply.github.com>
2019-09-05 17:48:45 +0300

    minor update/ code decrease

--- Tomb of Innahilation/main.js
+++ Tomb of Innahilation/main.js
@@ -11,20 +11,21 @@ function displayNpc(){
     getPicture.onmouseout = function(){
         getPicture.style.backgroundSize = "100%";
     } 
+
+    
 }
 
 
 function changeLocPic( currentLocation, locationReview){
+    buttonBack();
     
     //add locations where you want to generate random npc
     if(currentLocation === Port){
       currentLocation.npc =  randomNpc();
       currentNpc=currentLocation.npc;
-      if(FindThief.isDone===false && BlackMarket.isActive===false){
-          currentNpc = Rapoza
-      }
-
-      
+     if(FindThief.isDone && BlackMarket.isActive===false){
+          currentNpc = Rapoza; 
+     }
     } else if (currentLocation === Market){
         currentLocation.npc = randomMerchant();
         currentNpc=currentLocation.npc;
@@ -32,10 +33,12 @@ function changeLocPic( currentLocation, locationReview){
     } else if (currentLocation === Tavern){
         currentLocation.npc = randomBarNpc();
         currentNpc = currentLocation.npc;
+        displayBartenderButton();
+        
     } else if (currentLocation === Racing){
         currentLocation.npc = randomNpc();
         currentNpc=currentLocation.npc;
-    }
+    } 
         
     
     //add backgroundloc
@@ -53,17 +56,20 @@ function changeLocPic( currentLocation, locationReview){
     displayNpc();
 //......................................quests shinanigans...............................................
     let talk = document.querySelector("#talk");
+   
     if (currentLocation === Tavern && currentLocation.npc === Beggar && FindThief.isActive){
         talk.innerHTML += "\n\n A beggar is asking you to come close to him...";
     }else if(currentNpc === Rapoza && currentLocation === Port){
         talk.innerHTML += "\n\n Stop! You are under arrest for stealing a dragon-cows from local farmers, selling shadow grass and riding a horse while being naked in a Tavern. You would be put in jail and judged according to a local laws. Can you say anything for your defence?";
         document.getElementById('go-button').disabled =true;
     }
+
 }
 
 
 // function below starts the game
 function startGame(){
+
     currentNpc = Sindra;
     currentLocation = Boat;
     let getButtons = document.querySelector("#choice");
@@ -77,65 +83,3 @@ function startGame(){
     talkFinish.innerHTML += '\n\n ....Suddenly you fill a strong push, barely catching the nearest barrell you prevent yourself from falling.  It seems that ship has arrived to port Nyanzaru. You can get to the port by going north...';
     }
  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
